b776114316ce2ddffa00bd49bf3653386f43a164
nazhG <nazhirgonzalez@gmail.com>
2021-05-16 01:08:59 -0400

    test changes

--- test/market.test.js
+++ test/market.test.js
@@ -1,9 +1,24 @@
 const Market = artifacts.require("Market");
+const tokenFactory = artifacts.require("GameItems");
 
 contract("Market", accounts => {
-	it("Get price ETH => USD", async function () {
-		const market = await Market.new();
-		const price = (await market.getThePrice()).toString();
-		console.log('\tPrice: ', price);
+	let token;
+	let market;
+	let price = 0;
+
+	before(async () => {
+		token = await tokenFactory.new();
+		market = await Market.new();
 	});
+	it("Get price ETH => USD", async function () {
+		const _price = (await market.getThePrice()).toString();
+		price = parseFloat(_price.slice(0, _price.length - 8) + '.' + _price.slice(_price.length - 8, _price.length));
+		//console.log('\tPrice: ', price);
+		assert.notEqual(price, 0);
+	});
+	// it("Create a offert", async function () {
+	// 	const market = await Market.new();
+	// 	const price = (await market.getThePrice()).toString();
+	// 	console.log('\tPrice: ', price);
+	// });
 });
--- test/nft.test.js
+++ test/nft.test.js
@@ -1,18 +1,41 @@
 const tokenFactory = artifacts.require("GameItems");
 const axios = require("axios");
 
-contract("NTF", accounts => {
+contract("NTF", ([owner, timmy]) => {
 	let token;
+
 	before(async () => {
 		token = await tokenFactory.new();
 	});
 	it("get Token", async function () {
 		const THORS_HAMMER = (await token.THORS_HAMMER()).toString();
-		const item = (await token.uri(THORS_HAMMER))
+		const uri_path = (await token.uri(THORS_HAMMER))
 			.toString()
 			.replace('{id}', THORS_HAMMER.padStart(64, "0"));
-		const { data } = await axios.get(item);
-		for (tokenID in { THORS_HAMMER: 0 })
-			console.log("\t", tokenID, "\n", data);
+		const { data } = await axios.get(uri_path);
+
+		uri_local = {
+			description: "GameItems - Thor's Hammer",
+			external_url: "https://forum.openzeppelin.com",
+			image: "https://openmoji.org/data/color/svg/1F528.svg",
+			name: "Thor's Hammer"
+		};
+
+		const isUriGet = JSON.stringify(data) == JSON.stringify(uri_local);
+
+		// for (tokenID in { THORS_HAMMER: 0 })
+		// 	console.log("\t", tokenID, "\n", data);
+
+		assert.isTrue(isUriGet);
+	});
+	it("Owner balance", async function () {
+		const SILVER = (await token.SILVER()).toString();
+		const balanceOwner = (await token.balanceOf(owner, SILVER)).toString();
+		assert.equal(balanceOwner, 20);
+	});
+	it("Other person balance", async function () {
+		const GOLD = (await token.GOLD()).toString();
+		const balance = (await token.balanceOf(timmy, GOLD)).toString();
+		assert.equal(balance, 0);
 	});
 });
