6917a63234fb0aa1719efcee9484ba273c897c3b
nazhG <nazhirgonzalez@gmail.com>
2021-05-16 13:22:38 -0400

    Make an offer

--- test/market.test.js
+++ test/market.test.js
@@ -10,15 +10,45 @@ contract("Market", accounts => {
 		token = await tokenFactory.new();
 		market = await Market.new();
 	});
+
 	it("Get price ETH => USD", async function () {
 		const _price = (await market.getThePrice()).toString();
 		price = parseFloat(_price.slice(0, _price.length - 8) + '.' + _price.slice(_price.length - 8, _price.length));
 		//console.log('\tPrice: ', price);
 		assert.notEqual(price, 0);
 	});
-	// it("Create a offert", async function () {
-	// 	const market = await Market.new();
-	// 	const price = (await market.getThePrice()).toString();
-	// 	console.log('\tPrice: ', price);
-	// });
+
+	it("Make an offer", async function () {
+
+		const shield = Number(await token.SHIELD());
+		const amount = (await token.balanceOf(accounts[0], shield)).toString();
+		const fiveMinutesOffer = Math.round(Date.now() / 1000) + (60 * 5);
+		const gameAddress = token.address;
+
+		const offer = {
+			token: gameAddress,
+			tokenID: shield,
+			amount: amount,
+			deadline: fiveMinutesOffer,
+			price: 2000
+		};
+
+		const tx = await market.MakeOffer(offer);
+		const _offer0 = await market.offers(0);
+
+		const offer0 = {
+			token: _offer0.token,
+			tokenID: Number(_offer0.tokenID),
+			amount: (_offer0.amount).toString(),
+			deadline: Number(_offer0.deadline),
+			price: Number(_offer0.price)
+		};
+
+		console.log('\tGas used: ', tx.receipt.gasUsed);
+		const isOfferSet = JSON.stringify(offer0) == JSON.stringify(offer);
+
+		assert.isTrue(isOfferSet);
+	});
+
+
 });
