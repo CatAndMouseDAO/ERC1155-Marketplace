c8202b0a0901fadfb19eabe4662e4753bd7e4353
w <w@cheesedao.xyz>
2021-12-04 17:03:43 -0500

    some new tests

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,47 +1,33 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  const [deployer, dao] = await ethers.getSigners();
+  const [deployer, dao, buyer] = await ethers.getSigners();
 
-  // Large number for approvals
-  const largeApproval = '100000000000000000000000000000000';
-  // Initial mint for wsCHEEZ
-  const initialMint = '10000000000000000000000000';
+		// Large number for approvals
+		const largeApproval = '100000000000000000000000000000000';
+		// Initial mint for wsCHEEZ
+		const initialMint = '10000000000000000000000000';
+	
+		// Deploy DAI
+		console.log("deploy wsCHEEZ")
+		const WSCHEEZ = await ethers.getContractFactory('WSCHEEZ');
+		const wsCHEEZ = await WSCHEEZ.deploy( 0 );
+		await wsCHEEZ.mint( buyer.address, initialMint );
+	
+		console.log("deploy NFT")
+		const NFT = await ethers.getContractFactory('NFT');
+		const nft = await NFT.deploy()
+		await nft.mint(initialMint)
+	
+		console.log("deploy market")
+		const Market = await ethers.getContractFactory('Market')
+		const market = await Market.deploy(wsCHEEZ.address, dao.address)
+		
+		console.log("wsCHEEZ: ", wsCHEEZ.address )
+		console.log("NFT: ", nft.address )
+		console.log("Market: ", market.address )
+		console.log("DAO: ", dao.address )
 
-  // Deploy DAI
-  console.log("deploy wsCHEEZ")
-  const WSCHEEZ = await ethers.getContractFactory('WSCHEEZ');
-  const wsCHEEZ = await WSCHEEZ.deploy( 0 );
-  await wsCHEEZ.mint( deployer.address, initialMint );
-
-  console.log("deploy NFT")
-  const NFT = await ethers.getContractFactory('NFT');
-  const nft = await NFT.deploy()
-  await nft.mint(initialMint)
-
-  console.log("deploy market")
-  const Market = await ethers.getContractFactory('Market')
-  const market = await Market.deploy(wsCHEEZ.address, dao.address)
-
-  console.log("deploy approval")
-  await nft.setApprovalForAll(market.address, true);
-  await wsCHEEZ.approve(market.address, largeApproval);
-
-  console.log("wsCHEEZ: ", wsCHEEZ.address )
-  console.log("NFT: ", nft.address )
-  console.log("Market: ", market.address )
-  console.log("DAO: ", dao.address )
-
-  console.log("Make offer")
-  let offer = {
-    token: nft.address,
-    tokenID: 0,
-    amount: 1,
-    deadline: Math.floor((Date.now()/1000)) + 100000,
-    price: parseInt(ethers.utils.parseUnits("3").toString())
-  }
-  console.log(offer)
-  await market.MakeOffer(offer)
 }
 
 main()
--- scripts/make_offer.js
+++ scripts/make_offer.js
@@ -1,41 +1,43 @@
-const { expect } = require("chai");
+
+/*
+wsCHEEZ:  0xa513E6E4b8f2a923D98304ec87F64353C4D5C853
+NFT:  0x8A791620dd6260079BF849Dc5567aDC3F2FdC318
+Market:  0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e
+DAO:  0x70997970C51812dc3A010C7d01b50e0d17dc79C8
+*/
+
 const { ethers } = require("hardhat");
 const { expectRevert,time } = require('@openzeppelin/test-helpers');
 
-describe("Token contract", function () {
-	it("Deploy and list", async function () {
-		const [deployer, dao, buyer] = await ethers.getSigners();
+
+async function main() {
+  const [deployer, dao, buyer] = await ethers.getSigners();
 
 		// Large number for approvals
 		const largeApproval = '100000000000000000000000000000000';
 		// Initial mint for wsCHEEZ
 		const initialMint = '10000000000000000000000000';
+        const wSCHEEZ_contract = '0xa513E6E4b8f2a923D98304ec87F64353C4D5C853'
+        const nft_contract = '0x8A791620dd6260079BF849Dc5567aDC3F2FdC318'
+        const market_contract = '0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e'
 	
 		// Deploy DAI
-		console.log("deploy wsCHEEZ")
+		console.log("attach wsCHEEZ")
 		const WSCHEEZ = await ethers.getContractFactory('WSCHEEZ');
-		const wsCHEEZ = await WSCHEEZ.deploy( 0 );
-		await wsCHEEZ.mint( deployer.address, initialMint );
+		const wsCHEEZ = await WSCHEEZ.attach(wSCHEEZ_contract);
 	
 		console.log("deploy NFT")
 		const NFT = await ethers.getContractFactory('NFT');
-		const nft = await NFT.deploy()
-		await nft.mint(initialMint)
+		const nft = await NFT.attach(nft_contract)
 	
 		console.log("deploy market")
 		const Market = await ethers.getContractFactory('Market')
-		const market = await Market.deploy(wsCHEEZ.address, dao.address)
-		
-		console.log("wsCHEEZ: ", wsCHEEZ.address )
-		console.log("NFT: ", nft.address )
-		console.log("Market: ", market.address )
-		console.log("DAO: ", dao.address )
-	
-		console.log("Set offer")
+		const market = await Market.attach(market_contract)
+
+
+        console.log("Set offer")
 		const fiveMinutesOffer = Number(await time.latest()) + ( 5 * 60000 );
-		const price = ethers.utils.parseUnits("3").toString()		
-		const tokens_to_transfer = ethers.utils.parseUnits("1000000").toString()		
-		await wsCHEEZ.transfer(buyer.address, tokens_to_transfer);
+		const price = ethers.utils.parseUnits("3").toString()
 
 		console.log("Setup approval for nft")
 		await nft.setApprovalForAll(market.address, true);
@@ -46,8 +48,10 @@ describe("Token contract", function () {
 
 		let nftBal = await nft.balanceOf(buyer.address, 0)
 		console.log("Buyer NFT bal before: ", nftBal.toString())
+
 		console.log("Approve tokens and Buy offer")
 		await wsCHEEZ.connect(buyer).approve(market.address, largeApproval);
+
 		let offer_id = 0 
 		let ammount = 1 
 		await market.connect(buyer).BuyOffer(offer_id, ammount);
@@ -59,5 +63,11 @@ describe("Token contract", function () {
 		nftBal = await nft.balanceOf(buyer.address, token_id)
 		console.log("buyer NFT bal after: ", nftBal.toString())
 
-	});
-});
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
