54594856a224eb711d819e2b56fe99468ee92ca9
RubeRoyceJr <ruberoycejr@protonmail.com>
2021-12-04 16:36:12 -0500

    proof of swap

--- test/market.test.js
+++ test/market.test.js
@@ -4,7 +4,7 @@ const { expectRevert,time } = require('@openzeppelin/test-helpers');
 
 describe("Token contract", function () {
 	it("Deploy and list", async function () {
-		const [deployer, dao] = await ethers.getSigners();
+		const [deployer, dao, buyer] = await ethers.getSigners();
 
 		// Large number for approvals
 		const largeApproval = '100000000000000000000000000000000';
@@ -25,19 +25,29 @@ describe("Token contract", function () {
 		console.log("deploy market")
 		const Market = await ethers.getContractFactory('Market')
 		const market = await Market.deploy(wsCHEEZ.address, dao.address)
-	
-		console.log("deploy approval")
-		await nft.setApprovalForAll(market.address, true);
-		await wsCHEEZ.approve(market.address, largeApproval);
-	
+		
 		console.log("wsCHEEZ: ", wsCHEEZ.address )
 		console.log("NFT: ", nft.address )
 		console.log("Market: ", market.address )
 		console.log("DAO: ", dao.address )
 	
-		console.log("Make offer")
+		console.log("Set offer")
 		const fiveMinutesOffer = Number(await time.latest()) + ( 5 * 60000 );
-		const price = ethers.utils.parseUnits("3").toString()
+		const price = ethers.utils.parseUnits("3").toString()		
+		await wsCHEEZ.transfer(buyer.address, price);
+
+		console.log("Setup approval for nft")
+		await nft.setApprovalForAll(market.address, true);
+		console.log("Make Offer")
 		await market.MakeOffer(nft.address, 0, 1, fiveMinutesOffer, price)
+
+		let nftBal = await nft.balanceOf(buyer.address, 0)
+		console.log("Buyer NFT bal before: ", nftBal.toString())
+		console.log("Approve tokens and Buy offer")
+		await wsCHEEZ.connect(buyer).approve(market.address, largeApproval);
+		await market.connect(buyer).BuyOffer(0, 1);
+		nftBal = await nft.balanceOf(buyer.address, 0)
+		console.log("buyer NFT bal after: ", nftBal.toString())
+
 	});
 });
